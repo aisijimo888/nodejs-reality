a5c2c3b4c3a653b164093a14d44fba393cc4d5a8
aisijimo888 <127577828+aisijimo888@users.noreply.github.com>
2024-06-16 16:57:52 +0800

    Update start.sh

--- start.sh
+++ start.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 export UUID=${UUID:-'f6bf5ff2-023f-4c00-a572-22bbebae4fbc'}
-export SERVER_PORT="${SERVER_PORT:-${PORT:-7860}}"
+export SERVER_PORT="${SERVER_PORT:-${PORT:-}}"
 export NEZHA_SERVER=${NEZHA_SERVER:-'nz.abc.cn'} 
 export NEZHA_PORT=${NEZHA_PORT:-'5555'}     
 export NEZHA_KEY=${NEZHA_KEY:-''}  
